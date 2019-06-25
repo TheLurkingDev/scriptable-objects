07e15fafeaf3aeb1c26dca7d252c3aeca40db456
Darren Cook <thelurkingdev@gmail.com>
2019-06-24 21:37:45 -0400

    Added ui text to display player's hitpoints. Note that the display does not change when PlayerHP value changes.

---
+++ Assets/Scriptable Objects/PlayerText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScriptableObjects
+{
+    public class PlayerText : MonoBehaviour
+    {
+        [SerializeField]
+        private FloatVariable _playerHp;
+        private Text _playerText;
+
+        private void Start()
+        {
+            _playerText = GetComponent<Text>();
+            if(_playerHp != null) { _playerText.text = "HP: " + _playerHp.Value.ToString();  }
+        }
+    }
+}
